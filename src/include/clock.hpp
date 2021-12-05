db50b7cefb07dd7f9ca6501b35c533e0f7440ea6
tomblack <tomblackwhite@qq.com>
2021-12-06 01:33:46 +0800

    新建时钟模拟,可以根据cycle数量模拟执行每个指令的时间

--- src/include/clock.hpp
+++ src/include/clock.hpp
@@ -1,1 +1,51 @@
 #pragma once
+#include <chrono>
+
+using namespace std::chrono;
+/*
+ * 需要根据cycle的数量自行推进当前时间
+ * 如果一条指令的时间实际执行的时间超过cycle数量的时间，立即返回，
+ * 否则，消耗当前时间使总时间达到指令cycle的时间。返回，也就是只是用来
+ * 同步一个函数的时钟。
+ *
+ */
+class Clock {
+private:
+  // NTSC NES CPU Cycle
+  using NESCPUCycle = std::ratio<1, 1789773>;
+  using CPUCycle = std::chrono::duration<int, NESCPUCycle>;
+
+  using CPUCycleTimePoint =
+      std::chrono::time_point<std::chrono::steady_clock, CPUCycle>;
+
+  CPUCycleTimePoint m_startTimePoint;
+
+  /*
+   * 返回当前时间点
+   */
+  CPUCycleTimePoint Now() {
+    return time_point_cast<CPUCycle>(steady_clock::now());
+  }
+
+public:
+  Clock(){
+
+  };
+
+  /*
+   * 开始计时
+   */
+  void Start() { m_startTimePoint = this->Now(); }
+
+  /*
+   * 传参cyclecount
+   * 从开始到结束，时间走了对应的cycle数量的时间就返回
+   */
+  void End(int cylceCount) {
+    int count = 0;
+    do {
+      auto now = this->Now();
+      count = (now - m_startTimePoint).count();
+    } while (count < cylceCount);
+  }
+};
--- test/main.cpp
+++ test/main.cpp
@@ -1,5 +1,25 @@
-#include <iostream>
-int main()
-{
-    std::cout << "hello world success" << std::endl;
+#include "clock.hpp"
+#include <chrono>
+using namespace std::chrono;
+#define BOOST_TEST_MODULE My Test
+#include <boost/test/included/unit_test.hpp>
+
+
+BOOST_AUTO_TEST_CASE(first_test) {
+  Clock clock;
+  auto start = std::chrono::high_resolution_clock().now();
+  clock.Start();
+
+  clock.End(10);
+  auto end = std::chrono::high_resolution_clock().now();
+  auto timeLength = duration_cast<nanoseconds>(end - start).count();
+  int nansMin = 550;
+  int nansMax = 570;
+
+  BOOST_TEST_MESSAGE("test start first 时间" << timeLength <<" " << nansMin*10 << " " << nansMax*10);
+  bool test = (timeLength <= nansMax * 10) && (timeLength >= nansMin * 10);
+
+  int i = 1;
+  BOOST_TEST(i);
+  BOOST_TEST(test);
 }
