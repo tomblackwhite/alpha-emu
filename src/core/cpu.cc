c6bc440189e387864f5ee566c448da2a0f67aca1
tomblack <tomblackwhite@qq.com>
2021-12-14 01:04:44 +0800

    修改插入指令方法

--- src/core/cpu.cc
+++ src/core/cpu.cc
@@ -1,4 +1,5 @@
 #include "../include/cpu.hh"
+#include <sys/endian.h>
 
 void CPU::InitInstructionSet() {
 
@@ -20,15 +21,15 @@ void CPU::InitInstructionSet() {
       前两个同或。也就是前两个符号相同。也就是前两个等价。这样两个等价的符号，在后面的逻辑运算中，可以
       互相替换。
     */
-    auto ac=this->m_AC;
-    bool overFlowFlag=(~(ac^memoryValue))&(memoryValue^result)&0x80;
-    this->OverflowFlag()=overFlowFlag;
+    auto ac = this->m_AC;
+    bool overFlowFlag = (~(ac ^ memoryValue)) & (memoryValue ^ result) & 0x80;
+    this->OverflowFlag() = overFlowFlag;
 
-    this->NegativeFlag()=result&0x80;
-    this->CarryFlag()=result&0x0100;
-    this->ZeroFlag()=!(result&0xFF);
+    this->NegativeFlag() = result & 0x80;
+    this->CarryFlag() = result & 0x0100;
+    this->ZeroFlag() = !(result & 0xFF);
 
-    this->m_AC=result&0xFF;
+    this->m_AC = result & 0xFF;
   };
-  this->m_instructionSet.insert({0x69,{2,0x69,adc}});
+  m_instructionMap.insert({0x69,{2,0x69,AddressMode::immediate,adc}});
 }
