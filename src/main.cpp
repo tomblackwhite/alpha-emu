b4cd34c32cfecbafb639198b67e967ba7fc1fbd7
tomblack <tomblackwhite@qq.com>
2022-03-15 03:15:01 +0800

    vulkan

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,39 @@
 #include "file.hh"
 #include "log.hh"
-#include <cstddef>
-#include <cstdlib>
-#include <filesystem>
-#include <ios>
 #include "mainwindow.hh"
 #include <QApplication>
-int main(int argc , char *argv[]) {
-  const auto log = Log::GetInstance();
-  log.info("hello world");
-  std::string homePath = std::getenv("HOME");
-  log.info(homePath);
-  filesystem::path filePath = homePath + "/MARIO.NES";
-  File file;
-  file.Read(filePath);
-  std::cout << file.m_header.size() << ' ' << file.m_file.size()  << ' '
-            << file.m_trainer.size() + file.m_RPGRom.size() +
-                   file.m_CHRRom.size();
+#include "triangle.hh"
+// int main(int argc , char *argv[]) {
+//   const auto log = Log::GetInstance();
+//   log.info("hello world");
+//   std::string homePath = std::getenv("HOME");
+//   log.info(homePath);
+//   filesystem::path filePath = homePath + "/MARIO.NES";
+//   File file;
+//   file.Read(filePath);
+//   std::cout << file.m_header.size() << ' ' << file.m_file.size()  << ' '
+//             << file.m_trainer.size() + file.m_RPGRom.size() +
+//                    file.m_CHRRom.size();
 
-  QApplication app(argc,argv);
+//   QApplication app(argc,argv);
 
-  MainWindow w;
-  w.show();
-  return app.exec();
+//   MainWindow w;
+//   w.show();
+//   return app.exec();
+// }
+
+
+
+int main() {
+
+  HelloTriangleApplication app;
+  try {
+    std::cout << "success run\n";
+    app.run();
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
 }
